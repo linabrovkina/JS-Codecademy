1a506740d7bbc07792ce271f3679fb4dd68cebd0
linabrovkina <brovkinalina3@gmail.com>
2024-06-24 19:08:39 -0400

    Add Magic Eight Ball project

--- script.js
+++ script.js
@@ -27,3 +27,61 @@ let myAgeInDogYears = earlyYears + laterYears;
 let myName = 'Angelina'.toLowerCase();
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+//Magic Eight Ball
+
+let userName = 'Lina';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+const userQuestion = 'Will I have a dog?';
+console.log(`${userName} has asked - ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+if(randomNumber === 0){
+  eightBall = 'It is certain';
+} else if (randomNumber === 1){
+  eightBall = 'It is decidedly so';
+} else if (randomNumber === 2){
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3){
+  eightBall = 'Cannot predict now';
+} else if (randomNumber === 4){
+  eightBall = 'Do not count on it';
+} else if (randomNumber === 5){
+  eightBall = 'My sources say no';
+} else if (randomNumber === 6){
+  eightBall = 'Outlook not so good';
+} else if (randomNumber === 7){
+  eightBall = 'Signs point to yes';
+}
+
+console.log(`The Magic Eight Ball says, ${eightBall}`);
+
+// Swith case
+
+switch(randomNumber) {
+    case 0:
+      eightBall = 'It is certain';
+      break;
+    case 1:
+      eightBall = 'It is decidedly so';
+      break;
+    case 2:
+      eightBall = 'Reply hazy try again';
+      break;
+    case 3:
+      eightBall = 'Cannot predict now';
+      break;
+    case 4:
+      eightBall = 'Do not count on it';
+      break;
+    case 5:
+      eightBall = 'My sources say no';
+      break;
+    case 6:
+      eightBall = 'Outlook not so good';
+      break;
+    case 7:
+      eightBall = 'Signs point to yes';
+      break;
+  }
